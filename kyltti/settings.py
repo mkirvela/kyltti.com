688198659fcc6976a080d8ca775efe50d34d1be9
Matias Kirvelä <matias.kirvela@futurice.com>
2012-07-18 15:39:49 +0300

    added admin

--- kyltti/settings.py
+++ kyltti/settings.py
@@ -126,7 +126,7 @@ INSTALLED_APPS = (
     'gunicorn',
     'api',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
