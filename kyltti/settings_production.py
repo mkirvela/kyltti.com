a4ffd9bf6a2cb43b9309cd345f422be2d6c43d6a
Matias Kirvelä <matias.kirvela@futurice.com>
2012-08-07 12:28:15 +0300

    testing new settings and urls

--- kyltti/settings_production.py
+++ kyltti/settings_production.py
@@ -1,2 +1,4 @@
 import dj_database_url
+DEBUG = False
+MEDIA_URL = 'http://kyltti.s3.amazonaws.com/'
 DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
--- kyltti/urls.py
+++ kyltti/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 import settings
 # Uncomment the next two lines to enable the admin:
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
 admin.autodiscover()
 
@@ -18,3 +19,5 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
+
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
