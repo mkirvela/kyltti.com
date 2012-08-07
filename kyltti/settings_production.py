2283151f471f7e653629c781d365427da78c394c
Matias Kirvelä <matias.kirvela@futurice.com>
2012-08-07 14:31:14 +0300

    testing different settings

--- kyltti/settings_production.py
+++ kyltti/settings_production.py
@@ -1,4 +1,5 @@
 import dj_database_url
 DEBUG = False
 MEDIA_URL = 'http://kyltti.s3.amazonaws.com/'
+STATIC_URL = '/static/'
 DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
